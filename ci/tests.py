c2d75e8c26e0987957f70ca5e95e42616fb5f551
github action <lars.buntemeyer@gmail.com>
2022-07-13 13:27:32 +0200

    added PrePARE test

--- ci/tests.py
+++ ci/tests.py
@@ -1,16 +1,47 @@
 import os
+import shutil
+import subprocess
 
 import cordex as cx
 import pyremo as pr
-
-# import pytest
-import xarray as xr
+import pytest
 from pyremo import cmor as prcmor
 
 table_dir = "./Tables"
 
 
-def test_cmorizer_fx():
+def copy_tables(table_dir):
+    """copy CORDEX-CMIP6 table names to CMIP6 tables names
+
+    This is because PrePARE does not allow us to give tables names.
+
+    """
+    import glob
+
+    tables = glob.glob(os.path.join(table_dir, "*"))
+    new_tables = []
+    for src in tables:
+        dst_path = os.path.dirname(src)
+        base = os.path.basename(src)
+        dst = os.path.join(dst_path, "CMIP6_" + "_".join(base.split("_")[1:]))
+        # os.symlink(src, dst)
+        shutil.copyfile(src, dst)
+        new_tables.append(dst)
+    return new_tables
+
+
+def copy_filename_to_cmip6(filename):
+    base = os.path.basename(filename)
+    path = os.path.dirname(filename)
+    elements = base.split("_")
+    del elements[3]
+    cmip6_filename = os.path.join(path, "_".join(elements))
+    shutil.copyfile(filename, cmip6_filename)
+    return cmip6_filename
+
+
+@pytest.fixture
+def fx_file():
     ds = pr.data.surflib("EUR-11")
     eur11 = cx.cordex_domain("EUR-11")
     ds = ds.assign_coords({"lon": eur11.lon, "lat": eur11.lat})
@@ -24,8 +55,21 @@ def test_cmorizer_fx():
         time_units=None,
         allow_units_convert=True,
     )
-    output = xr.open_dataset(filename)
-    assert "orog" in output
+    return filename
+
+
+def test_fx(fx_file):
+    new_tables = copy_tables(table_dir)
+    fx_file = copy_filename_to_cmip6(fx_file)
+    print(fx_file)
+    command = ["PrePARE", "--table-path", table_dir, fx_file]
+    test = subprocess.run(command)
+    for f in new_tables:
+        os.remove(f)
+    print("The exit code was: %d" % test.returncode)
+    assert test.returncode == 0
+
+    # return os.system(f"PrePARE --table-path ./Tables {fx_file}")
 
 
 # def test_cmorizer_mon():
