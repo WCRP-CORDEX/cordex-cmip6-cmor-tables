8888f973a3f57b307a1b508e61e3cc4f01558018
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2022-10-20 17:36:04 +0200

    added height and pressure coordinates

--- scripts/create_coordinates.py
+++ scripts/create_coordinates.py
@@ -1,4 +1,5 @@
 import json
+import re
 from os import path as op
 
 path = op.join(op.dirname(__file__), "../Tables")
@@ -106,12 +107,84 @@ latitude = {
 }
 
 
+time = {
+    "standard_name": "time",
+    "units": "days since ?",
+    "axis": "T",
+    "long_name": "time",
+    "climatology": "",
+    "formula": "",
+    "must_have_bounds": "yes",
+    "out_name": "time",
+    "positive": "",
+    "requested": "",
+    "requested_bounds": "",
+    "stored_direction": "increasing",
+    "tolerance": "",
+    "type": "double",
+    "valid_max": "",
+    "valid_min": "",
+    "value": "",
+    "z_bounds_factors": "",
+    "z_factors": "",
+    "bounds_values": "",
+    "generic_level_name": "",
+}
+
+time1 = {
+    "standard_name": "time",
+    "units": "days since ?",
+    "axis": "T",
+    "long_name": "time",
+    "climatology": "",
+    "formula": "",
+    "must_have_bounds": "no",
+    "out_name": "time",
+    "positive": "",
+    "requested": "",
+    "requested_bounds": "",
+    "stored_direction": "increasing",
+    "tolerance": "",
+    "type": "double",
+    "valid_max": "",
+    "valid_min": "",
+    "value": "",
+    "z_bounds_factors": "",
+    "z_factors": "",
+    "bounds_values": "",
+    "generic_level_name": "",
+}
+
+
 dim_table = {
     "longitude": longitude,
     "latitude": latitude,
-    "height": height,
-    "pressure": pressure,
+    "heightm": height,
+    "p": pressure,
+    "time": time,
+    "time1": time1,
 }
+
+
+def create_dimension_entry(dim):
+    dim_type = re.sub(r"[0-9]", "", dim)  # remove digits
+    if dim_type in ["heightm", "p"]:
+        value = get_value_from_str(dim)
+        return create_coord(dim_type, value)
+    return dim_table[dim].copy()
+
+
+def create_coord(dim_type, value):
+    coord_entry = dim_table[dim_type].copy()
+    coord_entry["value"] = str(100 * int(value)) if dim_type == "p" else value
+    return coord_entry
+
+
+def get_value_from_str(out_name):
+    try:
+        return re.search(r"\d+", out_name).group()
+    except Exception:
+        return None
 
 
 def dims_from_table(table):
@@ -142,10 +215,21 @@ def unique_dims_from_tables(tables):
     return unique_dims(dims)
 
 
+def table_to_json(table, dir=None):
+    if dir is None:
+        dir = "./"
+    filename = op.join(dir, "CORDEX_coordinate.json")
+    print(f"writing: {filename}")
+    with open(filename, "w") as fp:
+        json.dump(table, fp, indent=4)
+
+
 def run():
     dims = unique_dims_from_tables(files)
     print(f"found: {dims}")
+    return {"axis_entry": {d: create_dimension_entry(d) for d in dims}}
 
 
 if __name__ == "__main__":
-    run()
+    dims = run()
+    table_to_json(dims)
